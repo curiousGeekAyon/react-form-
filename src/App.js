6dc148d170ee8137719e7c208dcc31d559828b03
Ayon Sinha <ayon1234kgp@gmail.com>
2023-04-11 08:18:28 +0530

    version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  let[input,setInput]=useState({
+     fname:"",
+     lname:"",
+     email:""
+  });
+function takeInput(e)
+          {
+             let inputvalue=e.target.value;
+             let name=e.target.name;
+             setInput((preValue)=>{
+                if(name === "fName"){
+                  return {
+                    fname:inputvalue,
+                    lname:preValue.lname,
+                    email:preValue.email
+                   };
+                  }
+                else if(name==="lName")
+                {
+                  return{
+                    fname:preValue.fname,
+                    lname:inputvalue,
+                    email:preValue.email
+                  };
+                } 
+               else{
+                     return {
+                        fname:preValue.fname,
+                        lname:preValue.lname,
+                        email:inputvalue
+                     }
+               }   
+             });
+          }
+let[name,setName]=useState({
+    fname:"",
+    lname:""
+});
+function giveOutput(event)
+       {
+         event.preventDefault();
+         setName({
+             fname:input.fname,
+             lname:input.lname,
+             email:input.email
+         });
+       }          
+  return (<>
+     <form onSubmit={giveOutput}>
+     <div  className='wrapper'>
+    <div >
+    <h1 className='h1style'>hi, {name.fname} {name.lname}</h1>
+    <h1 className='h1style'>{name.email}</h1>
+    <input type="text" onInput={takeInput} placeholder='Enter your First Name' className='inputStyle' value={input.fname} name="fName"/>
+    <input type="text" onInput={takeInput} placeholder='Enter your Last Name' className='inputStyle' value={input.lname}name="lName"/>
+    <input type='email' onInput={takeInput} placeholder='Enter your email'  className='inputStyle'  value={input.email} name="email"/>
     </div>
+    <button type="submit">Click it👌</button>
+    </div>
+    </form>
+    </>
   );
 }
 
