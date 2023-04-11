d2c995e071402e0e7163a4bc4105d25093e33b8a
Ayon Sinha <ayon1234kgp@gmail.com>
2023-04-11 16:20:23 +0530

    used object destructuring to simplify hardcoding and reduce the code length

--- src/App.js
+++ src/App.js
@@ -11,34 +11,47 @@ function takeInput(e)
           {
              let inputvalue=e.target.value;
              let name=e.target.name;
-             setInput((preValue)=>{
-                if(name === "fName"){
-                  return {
-                    fname:inputvalue,
-                    lname:preValue.lname,
-                    email:preValue.email
-                   };
-                  }
-                else if(name==="lName")
-                {
-                  return{
-                    fname:preValue.fname,
-                    lname:inputvalue,
-                    email:preValue.email
-                  };
-                } 
-               else{
-                     return {
-                        fname:preValue.fname,
-                        lname:preValue.lname,
-                        email:inputvalue
-                     }
-               }   
-             });
+            // const{inputvalue,name}=e.target;
+            // console.log(e.target);
+            setTimeout(() => {
+              setInput((preValue)=>{
+                //   if(name === "fName"){
+                //     return {
+                //       fname:inputvalue,
+                //       lname:preValue.lname,
+                //       email:preValue.email
+                //      };
+                //     }
+                //   else if(name==="lName")
+                //   {
+                //     return{
+                //       fname:preValue.fname,
+                //       lname:inputvalue,
+                //       email:preValue.email
+  
+                //     };
+                //   } 
+                //  else{
+                //        return {
+                //           fname:preValue.fname,
+                //           lname:preValue.lname,
+                //           email:inputvalue
+  
+                //        }
+                //  } 
+                return {
+                  ...preValue, //used spread operator to clone the preValue object 
+                  [name]:inputvalue //updated the desired key's value of preValue  using name as index to acces the key stored in name and updated it's value by the current value
+                }
+      
+               })
+            }, 400);
+         ;
           }
 let[name,setName]=useState({
     fname:"",
-    lname:""
+    lname:"",
+    email:""
 });
 function giveOutput(event)
        {
@@ -48,6 +61,7 @@ function giveOutput(event)
              lname:input.lname,
              email:input.email
          });
+         console.log(input.fname);
        }          
   return (<>
      <form onSubmit={giveOutput}>
@@ -55,9 +69,10 @@ function giveOutput(event)
     <div >
     <h1 className='h1style'>hi, {name.fname} {name.lname}</h1>
     <h1 className='h1style'>{name.email}</h1>
-    <input type="text" onInput={takeInput} placeholder='Enter your First Name' className='inputStyle' value={input.fname} name="fName"/>
-    <input type="text" onInput={takeInput} placeholder='Enter your Last Name' className='inputStyle' value={input.lname}name="lName"/>
+    <input type="text" onInput={takeInput} placeholder='Enter your First Name' className='inputStyle' value={input.fname} name="fname"/>
+    <input type="text" onInput={takeInput} placeholder='Enter your Last Name' className='inputStyle' value={input.lname}name="lname"/>
     <input type='email' onInput={takeInput} placeholder='Enter your email'  className='inputStyle'  value={input.email} name="email"/>
+    {/* <input type='mobile' onInput={takeInput} placeholder='Enter your mobile no'  className='inputStyle'  value={input.email} name="email"/> */}
     </div>
     <button type="submit">Click it👌</button>
     </div>
